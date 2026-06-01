51333805661c91a127c2ced1efa2724223b2d031
theRoone <theroonekz@gmail.com>
2026-06-01 23:00:21 +0500

    Initial commit: SQL scripts

--- Update_Dublicate_NeftehimLTD_to_PKON.sql
+++ Update_Dublicate_NeftehimLTD_to_PKON.sql
@@ -4,10 +4,8 @@
 DECLARE @CaseID INT = {0}; -- C# подставит сюда ID расчета
 DECLARE @CaseTypeName NVARCHAR(100) = 'Суточный'; -- тип (напр. 'Суточный')
 
--- Шаг 2: Начинаем общую транзакцию для всех операций
 BEGIN TRAN;
 
--- Шаг 3: Создание "таблицы-инструкции" для всех задач
 DECLARE @Tasks TABLE (
 	TaskID int IDENTITY(1,1) PRIMARY KEY,
 	-- Параметры для ПОИСКА значения (откуда берем цифру)
@@ -16,20 +14,16 @@ DECLARE @Tasks TABLE (
 	Target_SourceID int, Target_DestID int, Target_ProductSourceID int, Target_ProductDestID int
 );
 
--- #############################################################################
--- ## Шаг 4: ЗАПОЛНИТЬ ТАБЛИЦУ НЕОБХОДИМЫМИ ЗНАЧЕНИЯМИ                        ##
--- ## Добавлять новые строки INSERT INTO для каждой новой операции            ##
--- #############################################################################
 
 INSERT INTO @Tasks (
 	Source_ObjectID, Source_FlowSourceID, Source_ProductSourceID, Source_FlowDestID, Source_ProductDestID,
 	Target_SourceID, Target_DestID, Target_ProductSourceID, Target_ProductDestID
 ) VALUES
--- # Из [Узел_ПКОН_Выход_FI-71F_Пропан-пропиленовая фракция] в [ЕЦ 16-22] #
+-- # Из [НАИМЕНОВАНИЕ УЗЛА 1] в [НАИМЕНОВАНИЕ РЕЗЕРВУАРА 1] #
 (3585, 3585, 357, 488, 357,   -- Откуда берем значение
  488, 10204, 357, 442),       -- Где будем менять
 
--- # Из [Узел_ПКОН_Выход_FI-82F_Бутан-бутиленовая фракция] в [ЕС 01,02,05,06] #
+-- # Из [НАИМЕНОВАНИЕ УЗЛА 2] в [НАИМЕНОВАНИЕ РЕЗЕРВУАРА 2] #
 (3585, 3585, 83, 434, 83,   -- Откуда берем значение
  434, 10193, 83, 351);--запятая вместо ;       -- Где будем менять
 
@@ -37,17 +31,11 @@ INSERT INTO @Tasks (
 --(9999, 9999, 888, 777, 888,   -- Откуда берем значение
 -- 666, 5555, 444, 333);       -- Где будем менять
 
--- #############################################################################
--- ## Дальше ничего менять не нужно. Скрипт все сделает сам.                  ##
--- #############################################################################
-
--- Шаг 5: Объявление переменных для цикла
 DECLARE @CurrentTaskID int = 1;
 DECLARE @MaxTaskID int = (SELECT MAX(TaskID) FROM @Tasks);
 DECLARE @NewValue numeric(38, 3);
 DECLARE @IdsToUpdate TABLE (Id int);
 
--- Основной цикл, который проходит по каждой задаче из таблицы @Tasks
 WHILE @CurrentTaskID <= @MaxTaskID
 BEGIN
 	PRINT 'Выполняется задача #' + CAST(@CurrentTaskID AS nvarchar(10)) + '...';
@@ -62,15 +50,13 @@ BEGIN
 		@t_src = Target_SourceID, @t_dest = Target_DestID, @t_psrc = Target_ProductSourceID, @t_pdest = Target_ProductDestID
 	FROM @Tasks WHERE TaskID = @CurrentTaskID;
 
-	-- 1. Получаем значение, которое нужно вставить
-	SELECT TOP 1 @NewValue = Measured FROM [MB4].[dbo].[PNHZ_OBJECTS_FLOW_DETAILS_VIEW]
+	SELECT TOP 1 @NewValue = Measured FROM [MB4].[dbo].[ENCRYPTEDVIEW]
 	WHERE CaseID = @CaseID AND CaseTypeName = @CaseTypeName AND ObjectID = @s_obj AND FlowSourceID = @s_fsrc
 		AND ProductSourceID = @s_psrc AND FlowDestID = @s_fdest AND ProductDestID = @s_pdest;
 
 	IF @NewValue IS NULL BEGIN
 		PRINT '  > ВНИМАНИЕ: Для задачи #' + CAST(@CurrentTaskID AS nvarchar(10)) + ' не найдено исходное значение. Пропускаем...';
 	END ELSE BEGIN
-		-- 2. Находим ID всех строк для обновления и сохраняем их
 		INSERT INTO @IdsToUpdate (Id)
 		SELECT T.Id FROM MB4.dbo.Transactions AS T
 		INNER JOIN dbo.Cases AS c ON c.id = t.caseid INNER JOIN dbo.Analyses AS a ON a.sfid = c.AnalysisSfId
@@ -85,11 +71,9 @@ BEGIN
 		IF NOT EXISTS (SELECT 1 FROM @IdsToUpdate) BEGIN
 				PRINT '  > ВНИМАНИЕ: Для задачи #' + CAST(@CurrentTaskID AS nvarchar(10)) + ' не найдено строк для обновления. Пропускаем...';
 		END ELSE BEGIN
-			-- 3. Обнуляем ВСЕ найденные строки
 			UPDATE T SET T.Measured = 0, T.Reconciled = 0
 			FROM MB4.dbo.Transactions AS T INNER JOIN @IdsToUpdate AS I ON T.Id = I.Id;
-
-			-- 4. Записываем новое значение в ОДНУ из этих строк (с наименьшим ID)
+		
 			UPDATE T SET T.Measured = @NewValue
 			FROM MB4.dbo.Transactions AS T WHERE T.Id = (SELECT MIN(Id) FROM @IdsToUpdate);
 
@@ -103,6 +87,5 @@ END
 PRINT '==================================================';
 PRINT 'Все задачи выполнены. Завершение транзакции...';
 
--- Шаг 6: Завершение транзакции.
 COMMIT TRAN;    -- Сохраняем все изменения, сделанные в цикле
 -- ROLLBACK TRAN;  -- Или откатываем, если нужно прервать
